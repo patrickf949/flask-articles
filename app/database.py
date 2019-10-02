0820ceb65175138036db7c5602cc3fe82959ad20
e-ian <ianemma70@gmail.com>
2019-10-02 21:08:16 +0300

    add initial commit for setting up flask app

---
+++ app/database.py
@@ -0,0 +1,26 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+
+class Database:
+
+    def __init__(self):
+        self.connect = psycopg2.connect(
+            dbname="karibu",
+            user="postgres",
+            password="P@55word",
+            host="localhost",
+            port="5432"
+        )
+        self.conn.autocommit = True
+        self.cur = self.conn.cursor()
+        self.dict_cursor = self.conn.cursor(cursor_factory=RealDictCursor)
+        self.create_user_table()
+
+        print("Connected to karibu")
+
+    def create_user_table(self):
+        user_table = "CREATE TABLE IF NOT EXISTS users(user_id serial PRIMARY KEY, username varchar(50), email varchar(30), password varchar(256))"
+
+        self.cur.execute(user_table)
